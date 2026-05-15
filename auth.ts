478e8a288b33dddf6467abadcdf43b493bd35f3a
Yentec <ypoulard.theuil+github@gmail.com>
2026-05-15 15:36:20 +0200

    feat(auth): configure nextauth with gitHub and resend + nextauth updated version

---
+++ auth.ts
@@ -0,0 +1,22 @@
+import NextAuth from "next-auth";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { db } from "@/lib/db";
+import authConfig from "./auth.config";
+
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  adapter: PrismaAdapter(db),
+  session: { strategy: "jwt" },
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) token["id"] = user.id;
+      return token;
+    },
+    async session({ session, token }) {
+      if (token["id"] && session.user) {
+        session.user.id = token["id"] as string;
+      }
+      return session;
+    },
+  },
+  ...authConfig,
+});
